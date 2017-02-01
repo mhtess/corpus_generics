8c217b93b52fa2f1f7c52863175a3eb5890891dd
Chakia Larie Hall-Watley <chakia@cardinal1.stanford.edu>
2017-01-31 17:07:19 -0800

    Sets up the generics experiment. Adds logic to render a question to the participant (from participant_questions.js), also adds logic that renders the according dependent measurement. Comments marked with TODO(chakia) are left in intentionally in order to note gaps and area that still need to be worked on for myself or anyone else.

---
+++ generics_experiment/template/js/participant_questions.js
@@ -0,0 +1,22 @@
+var questions = [
+    {question: "Are [plural noun] inanimate?", dependent_measure: "binary"},
+    {question: "What percentage of [plural noun] would you say [verb phrase]?", dependent_measure: "slider"},
+    {question: "Can you empirically prove the veracity of the statement?", dependent_measure: "binary"},
+    {question: "Is this statement observable?", dependent_measure: "binary"},
+    {question: "Are [plural noun] animals?", dependent_measure: "binary"},
+    {question: "Around how many [plural noun] would you say there are?", dependent_measure: "textbox"},
+    {question: "What percentage of [plural noun] would you say [verb phrase]", dependent_measure: "slider"},
+    {question: "Are [plural noun] pertaining to a specific group of people? (lawyers, teachers, etc.)", dependent_measure: "binary"},
+    {question: "Is this statement debatable?", dependent_measure: "binary"},
+]
+
+var random_questions = [];
+function  generate_random_questions(n) {
+    var i = 0;
+    while (i < n) {
+	var rand = questions[Math.floor(Math.random() * questions.length)];
+	random_questions.push(rand);
+	i++
+    }
+}
+
