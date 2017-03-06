e21e676fe8d7dc172f51c0816407d2b5336d5747
Charles Jacob Foster <cfoster0@cardinal2.stanford.edu>
2017-03-05 19:37:12 -0800

    Added people-specific domain restriction experiment

---
+++ experiments/domain-restriction-people/js/participant_questions.js
@@ -0,0 +1,14 @@
+var questions = [    
+    {question: "In the underlined statement, is the speaker talking about [noun phrase] in general or some specific group/kind of [noun phrase]?", dependent_measure: "multi"}
+]
+
+var random_questions = [];
+function  generate_random_questions(n) {
+    var i = 0;
+    while (i < n) {
+	var rand = questions[Math.floor(Math.random() * questions.length)];
+	random_questions.push(rand);
+	i++
+    }
+}
+
