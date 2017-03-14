f915745645aa1cea3d140351a72fc148d6f10771
Chakia Larie Hall-Watley <chakia@cardinal1.stanford.edu>
2017-03-14 14:09:53 -0700

    adds in data from MH's animal generic experiment to the emboldened training experiment

--- experiments/emboldened_training/js/stimuli.js
+++ experiments/emboldened_training/js/stimuli.js
@@ -19,89 +19,169 @@ var generate_stim = function() {
     }
     
     examples = [
-        ["<br><b>Speaker #1</b>: ... <br><b>Speaker #2</b>:",
-        "This is a sentence.",
-        "This",
-        "is a sentence.",
-        "is",
+        ["<br><b>Speaker #1</b>: What carries Lyme disease? <br><b>Speaker #2</b>:",
+        "Ticks carry Lyme disease.",
+        "Ticks",
+        "carry Lyme disease.",
+        "carry",
         "1"
         ],
 
-        ["<br><b>Speaker #1</b>: ... <br><b>Speaker #2</b>:",
-        "This is a sentence.",
-        "This",
-        "is a sentence.",
-        "is",
+        ["<br><b>Speaker #1</b>: What animal has a mane? <br><b>Speaker #2</b>:",
+        "Lions have manes.",
+        "Lions",
+        "have manes.",
+        "have",
         "2"
         ],
               
-        ["<br><b>Speaker #1</b>: ... <br><b>Speaker #2</b>:",
-        "This is a sentence.",
-        "This",
-        "is a sentence.",
-        "is",
+        ["<br><b>Speaker #1</b>: What animal has beautiful feathers? <br><b>Speaker #2</b>:",
+        "Peacocks have beautiful feathers.",
+        "Peacocks",
+        "have beautiful feathers.",
+        "have",
         "3"
         ],
         
-        ["<br><b>Speaker #1</b>: ... <br><b>Speaker #2</b>:",
-        "This is a sentence.",
-        "This",
-        "is a sentence.",
-        "is",
+        ["<br><b>Speaker #1</b>: What animal builds dams?  <br><b>Speaker #2</b>:",
+        "Beavers build dams.",
+        "Beavers",
+        "build dams..",
+        "build",
         "4"
         ],       
         
-        ["<br><b>Speaker #1</b>: ... <br><b>Speaker #2</b>:",
-        "This is a sentence.",
-        "This",
-        "is a sentence.",
-        "is",
+        ["<br><b>Speaker #1</b>: What animals are friendly? <br><b>Speaker #2</b>:",
+        "Dogs are friendly.",
+        "Dogs",
+        "are friendly.",
+        "are",
         "5"
-        ],       
-        
-        ["<br><b>Speaker #1</b>: ... <br><b>Speaker #2</b>:",
-        "This is a sentence.",
-        "This",
-        "is a sentence.",
-        "is",
+        ],   
+	
+	["<br><b>Speaker #1</b>: What animal flies? <br><b>Speaker #2</b>:",
+        "Ducks fly.",
+        "Ducks",
+        "fly.",
+        "fly",
         "6"
-        ],       
-        
-        ["<br><b>Speaker #1</b>: ... <br><b>Speaker #2</b>:",
-        "This is a sentence.",
-        "This",
-        "is a sentence.",
-        "is",
+        ],
+
+	["<br><b>Speaker #1</b>: What animal lays eggs? <br><b>Speaker #2</b>:",
+        "Birds lay eggs?.",
+        "Birds",
+        "lay eggs.",
+        "lay",
         "7"
-        ],       
-        
-        ["<br><b>Speaker #1</b>: ... <br><b>Speaker #2</b>:",
-        "This is a sentence.",
-        "This",
-        "is a sentence.",
-        "is",
-        "8"
-        ],       
-        
-        ["<br><b>Speaker #1</b>: ... <br><b>Speaker #2</b>:",
-        "This is a sentence.",
-        "This",
-        "is a sentence.",
-        "is",
+        ],
+
+        ["<br><b>Speaker #1</b>: What animal is white? <br><b>Speaker #2</b>:",
+        "Swans are white.",
+        "Swans",
+        "are white.",
+        "are",
+	 "8"
+        ],
+
+	["<br><b>Speaker #1</b>: What bites people? <br><b>Speaker #2</b>:",
+        "Mosquitos bite people.",
+        "Mosquitos",
+        "bite people.",
+        "bite",
         "9"
         ],
-        
-        ["<br><b>Speaker #1</b>: ... <br><b>Speaker #2</b>:",
-        "This is a sentence.",
-        "This",
-        "is a sentence.",
-        "is",
+
+        ["<br><b>Speaker #1</b>: What gives birth to live young? <br><b>Speaker #2</b>:",
+        "Whales give birth to live young.",
+        "Whales",
+        "give birth to live young.",
+        "give",
         "10"
+        ],
+
+	["<br><b>Speaker #1</b>: What disease do ticks carry? <br><b>Speaker #2</b>:",
+        "Ticks carry Lyme disease.",
+        "Ticks",
+        "carry Lyme disease.",
+        "carry",
+        "11"
+        ],
+
+        ["<br><b>Speaker #1</b>: What is a distinctive feature of lions? <br><b>Speaker #2</b>:",
+        "Lions have manes.",
+        "Lions",
+        "have manes.",
+        "have",
+        "12"
+        ],
+
+        ["<br><b>Speaker #1</b>: What do peacocks look like?  <br><b>Speaker #2</b>:",
+        "Peacocks have beautiful feathers.",
+        "Peacocks",
+        "have beautiful feathers.",
+        "have",
+        "13"
+        ],
+
+	["<br><b>Speaker #1</b>: What do beavers do?  <br><b>Speaker #2</b>:",
+        "Beavers build dams.",
+        "Beavers",
+        "build dams..",
+        "build",
+        "14"
+        ],
+
+        ["<br><b>Speaker #1</b>: What are dogs like? <br><b>Speaker #2</b>:",
+        "Dogs are friendly.",
+        "Dogs",
+        "are friendly.",
+        "are",
+        "15"
+        ],
+        
+        ["<br><b>Speaker #1</b>: How do ducks move around? <br><b>Speaker #2</b>:",
+        "Ducks fly.",
+        "Ducks",
+        "fly.",
+        "fly",
+        "16"
+        ],       
+        
+        ["<br><b>Speaker #1</b>: How do birds reproduce? <br><b>Speaker #2</b>:",
+        "Birds lay eggs?.",
+        "Birds",
+        "lay eggs.",
+        "lay",
+        "17"
+        ],       
+        
+        ["<br><b>Speaker #1</b>: What color are swans? <br><b>Speaker #2</b>:",
+        "Swans are white.",
+        "Swans",
+        "are white.",
+        "are",
+        "18"
+        ],       
+        
+        ["<br><b>Speaker #1</b>: What do mosquitos do? <br><b>Speaker #2</b>:",
+        "Mosquitos bite people.",
+        "Mosquitos",
+        "bite people.",
+        "bite",
+        "19"
+        ],
+        
+        ["<br><b>Speaker #1</b>: How do whales reproduce? <br><b>Speaker #2</b>:",
+        "Whales give birth to live young.",
+        "Whales",
+        "give birth to live young.",
+        "give",
+        "20"
         ]
     ]
 
     data = [];
-    for (var i=0; i < 10; i++) {
+    for (var i=0; i < 20; i++) {
         data.push(make_obj(examples[i]));
     }
     return data;
